8bb45be052150d6ac4a3d8b2dcfaa7acc399ab79
Victor dS <victordespirelt@gmail.com>
2026-01-06 14:46:55 +0100

    React Basic Workshop 2

---
+++ Workshop react basics 2/src/App.tsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import AvatarSimpsons from "./components/AvatarSimpsons";
+
+function App() {
+  const Bart = {
+    image: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
+    firstName: "Bart",
+    lastName: "Simpson",
+    city: "Springfield",
+    age: 15,
+  };
+  const Homer = {
+    image: "https://www.stickees.com/files/cartoon/the-simpsons/2246-homer-simpson-donut-2.png",
+    firstName: "Homer",
+    lastName: "Simpson",
+    city: "Springfield",
+    age: 55,
+  };
+
+  return (
+    <>
+      <AvatarSimpsons
+        image={Bart.image}
+        firstName={Bart.firstName}
+        lastName={Bart.lastName}
+        city={Bart.city}
+        age = {Bart.age}
+      />
+      <AvatarSimpsons
+        image={Homer.image}
+        firstName={Homer.firstName}
+        lastName={Homer.lastName}
+        city={Homer.city}
+        age = {Homer.age}
+
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ Workshop react basics 2/src/components/AvatarSimpsons.tsx
@@ -0,0 +1,24 @@
+interface AvatarSimpsonsProps {
+  firstName: string;
+  lastName: string;
+  city: string;
+  image: string;
+  age: number;
+}
+
+function AvatarSimpsons({ firstName, lastName, city, image, age }: AvatarSimpsonsProps) {
+  return (
+    <>
+
+      <img src={image} alt={`${firstName}, ${lastName}`} />
+
+      <p>
+        {`This is ${firstName} ${lastName}, he is ${age} and he lives in ${city}`}
+      </p>
+      
+    </>
+  );
+}
+
+export default AvatarSimpsons;
+
