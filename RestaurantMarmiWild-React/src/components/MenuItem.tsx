3c8f3eaa63195583016b6dfa3bfd437a3eede1cd
Victor dS <victordespirelt@gmail.com>
2026-01-08 14:46:49 +0100

    React Main Quest - MarmiWild

---
+++ RestaurantMarmiWild-React/src/components/MenuItem.tsx
@@ -0,0 +1,13 @@
+import MenuItem from './MenuItem';
+
+function MenuList({ foodItems }) {
+  console.log(foodItems);
+
+  return (
+    <>
+      {/* render a MenuItem component for each element of the foodItems array */}
+    </>
+  );
+}
+
+export default MenuList;
