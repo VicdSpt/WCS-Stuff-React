7c664619bb685ec326ed37824e754c21c1e893e4
Victor dS <victordespirlet@gmail.com>
2026-01-15 14:36:57 +0100

    Building my portfolio

---
+++ MyPortfolio/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+// import Header from "./component/Header"
+import Footer from "./component/Footer"
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Header /> */}
+      <Footer />
+    </>
+  )
+}
+
+export default App
