dc0e957717f7bb96999387b3a2f505fbac222531
Victor dS <victordespirlet@gmail.com>
2026-02-23 13:57:56 +0100

    Main Quest JS React & Express

--- AddToCart/AddtoCart/src/components/Header.tsx
+++ AddToCart/AddtoCart/src/components/Header.tsx
@@ -29,9 +29,7 @@ export const Header = () => {
             >
               Cart
               {cartCount > 0 && (
-                <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                  {cartCount}
-                </span>
+                <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">{cartCount}</span>
               )}
             </Link>
           </nav>
